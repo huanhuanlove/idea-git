611f2a724ba9114167e125bc86b3f316a26d8ecd
huanhuanlove <1468140258@qq.com>
2022-08-30 11:14:09 +0800

    102323 feat 修改接口

--- src/main/java/com/example/ideagit/controller/DemoController.java
+++ src/main/java/com/example/ideagit/controller/DemoController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * description 测试程序
+ * description
  *
- * @author mi_huaian.kang 2022/08/30 11:09
+ * @author mi_huaian.kang 2022/08/30 11:12
  */
 @Slf4j
 @RestController
 @RequestMapping("kang")
-public class KangController {
+public class DemoController {
 
     @PostMapping("demo1")
     public String demo1(String name) {
-        log.info("参数为：{}", name);
         return name;
     }
+
 }
